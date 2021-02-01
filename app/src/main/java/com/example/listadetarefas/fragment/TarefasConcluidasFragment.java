3133a26b2fe69bb8b86378c193b94adb790233fd
Gabriel Dümes <gabrieldumes@users.noreply.github.com>
2021-02-01 09:41:31 -0300

    Criação de abas

---
+++ app/src/main/java/com/example/listadetarefas/fragment/TarefasConcluidasFragment.java
@@ -0,0 +1,21 @@
+package com.example.listadetarefas.fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.listadetarefas.R;
+
+public class TarefasConcluidasFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_tarefas_concluidas, container, false);
+    }
+}
