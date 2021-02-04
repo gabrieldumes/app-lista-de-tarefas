ccff1fb90a0d5d9c701c3d051db2c2c09a55ba5f
Gabriel Dümes <gabrieldumes@users.noreply.github.com>
2021-02-04 06:37:04 -0300

    Evento de click em tarefas concluídas

--- app/src/main/java/com/example/listadetarefas/fragment/TarefasConcluidasFragment.java
+++ app/src/main/java/com/example/listadetarefas/fragment/TarefasConcluidasFragment.java
@@ -1,5 +1,8 @@
 package com.example.listadetarefas.fragment;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -10,10 +13,13 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.LinearLayout;
 
 import com.example.listadetarefas.ArmazenamentoBancoDeDados;
 import com.example.listadetarefas.R;
+import com.example.listadetarefas.RecyclerItemClickListener;
+import com.example.listadetarefas.activity.AlterarTarefaActivity;
 import com.example.listadetarefas.adapter.Adapter;
 import com.example.listadetarefas.model.Tarefa;
 
@@ -40,6 +46,44 @@ public class TarefasConcluidasFragment extends Fragment {
         recyclerTarefasConcluidas.setLayoutManager(layoutManager);
         recyclerTarefasConcluidas.setHasFixedSize(true);
         recyclerTarefasConcluidas.addItemDecoration(new DividerItemDecoration(getActivity(), LinearLayout.VERTICAL));
+
+        recyclerTarefasConcluidas.addOnItemTouchListener(
+                new RecyclerItemClickListener(
+                        getActivity(),
+                        recyclerTarefasConcluidas,
+                        new RecyclerItemClickListener.OnItemClickListener() {
+                            @Override
+                            public void onItemClick(View view, int position) {
+                                Tarefa tarefa = listaTarefas.get(position);
+                                Intent intent = new Intent(getActivity(), AlterarTarefaActivity.class);
+                                intent.putExtra("id_tarefa", tarefa.getId());
+                                startActivity(intent);
+                            }
+
+                            @Override
+                            public void onLongItemClick(View view, int position) {
+                                Tarefa tarefa = listaTarefas.get(position);
+                                AlertDialog.Builder dialog = new AlertDialog.Builder(getActivity());
+                                dialog.setTitle("Deseja excluir a tarefa?");
+                                dialog.setMessage("Esta operação não pode ser desfeita");
+                                dialog.setPositiveButton("Excluir", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        bancoDeDados.removerTarefa(tarefa.getId());
+                                        onStart();
+                                    }
+                                });
+                                dialog.setNegativeButton("Cancelar", null);
+                                dialog.create().show();
+                            }
+
+                            @Override
+                            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                            }
+                        }
+                )
+        );
 
         return view;
     }
